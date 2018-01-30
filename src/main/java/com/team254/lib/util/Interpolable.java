452e1f5b8c9c5de3cbc2c4501b03de71ed6ec2e4
Noah Gleason <noah.gleason1@gmail.com>
2018-01-30 16:54:46 -0500

    Fix javadoc

--- src/main/java/com/team254/lib/util/Interpolable.java
+++ src/main/java/com/team254/lib/util/Interpolable.java
@@ -10,14 +10,14 @@ package com.team254.lib.util;
 public interface Interpolable<T> {
     /**
      * Interpolates between this value and an other value according to a given parameter. If x is 0, the method should
-     * return this value. If x is 1, the method should return the other value. If 0 < x < 1, the return value should be
+     * return this value. If x is 1, the method should return the other value. If 0 is less than x is less than 1, the return value should be
      * interpolated proportionally between the two.
      *
      * @param other
      *            The value of the upper bound
      * @param x
      *            The requested value. Should be between 0 and 1.
-     * @return Interpolable<T> The estimated average between the surrounding data
+     * @return The estimated average between the surrounding data
      */
     public T interpolate(T other, double x);
 }
